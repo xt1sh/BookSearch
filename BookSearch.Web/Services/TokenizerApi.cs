9f6c11ea8a1fd5698e2e42567e9d8181d458b58a
xt1sh <azylav@gmail.com>
2024-12-18 06:45:27 +0200

    wip

---
+++ BookSearch.Web/Services/TokenizerApi.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSearch.Services
+{
+	public class TokenizerApi
+	{
+		private readonly HttpClient _httpClient;
+
+		public TokenizerApi(IHttpClientFactory httpClientFactory)
+		{
+			_httpClient = httpClientFactory.CreateClient();
+		}
+	}
+}
